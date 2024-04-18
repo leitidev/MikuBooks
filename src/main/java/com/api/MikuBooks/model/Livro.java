b1c5b9a42de8226aae235aae83ac65baa3278e4a
leitidev <leiticontato@hotmail.com>
2024-04-18 20:04:29 -0300

    Livro adicionado

---
+++ src/main/java/com/api/MikuBooks/model/Livro.java
@@ -0,0 +1,27 @@
+package com.api.MikuBooks.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Livro{
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "{livro.livro.notblank}")
+    private String nome;
+
+    private String autor;
+
+    private String editora;
+
+    private String paginas;
+
+}
