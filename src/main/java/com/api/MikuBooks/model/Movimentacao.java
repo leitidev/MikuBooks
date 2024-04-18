485f95f3c0211ac5120808577aff85b29902cb4b
leitidev <leiticontato@hotmail.com>
2024-04-18 20:31:42 -0300

    Movimentação atualizada

--- src/main/java/com/api/MikuBooks/model/Movimentacao.java
+++ src/main/java/com/api/MikuBooks/model/Movimentacao.java
@@ -1,6 +1,5 @@
 package com.api.MikuBooks.model;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import com.api.MikuBooks.validation.TipoMovimentacao;
@@ -9,10 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 
@@ -22,12 +18,6 @@ public class Movimentacao {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotBlank(message = "{movimentacao.descricao.notblank}")
-    @Size(min = 3, message = "{movimentacao.descricao.size}")
-    private String descricao;
-
-    @Positive(message = "{movimentacao.valor.positive}")
-    private BigDecimal valor;
 
     @PastOrPresent
     private LocalDate data;
